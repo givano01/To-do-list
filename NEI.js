52ef39d5b42a80cc9f3203a281e897ad2294ee42
Georgi Ivanov <gismacbook@Georgis-MacBook-Pro.local>
2020-11-16 10:29:46 +0100

    changes

--- NEI.js
+++
@@ -1,5 +0,0 @@
-module.exports.credentials = "postgres://hfhgqxgvoctgvk:c0bfe292f7253b7d05aec4729c15569abc2ed7ab9d67e2ea70ee353f1872bbbd@ec2-54-217-236-206.eu-west-1.compute.amazonaws.com:5432/df69du7ojdppie"
-
-module.exports.hashSecret = "hashSecretText";
-
-module.exports.tokenSecret = "tokenSecretText";
