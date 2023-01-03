7247c4cbdd42b99c3c5b1db83fd6c5b9e8e21bc3
Givano <ivanovsixx@yahoo.com>
2023-01-03 22:50:19 +0100

    updated

--- localenv.js
+++ localenv.js
@@ -1,4 +1,4 @@
-module.exports.credentials = "postgres://ayflpjqnnrjulj:e34e21766fe9a16a5879c81cb81e8211ba3c8d813cfca5c71e69dc739a9ff2f3@ec2-54-170-109-0.eu-west-1.compute.amazonaws.com:5432/df7gurh7ti4tfd"
+module.exports.credentials = "postgres://mnvgcqtmfbedlu:016656f1c2af47faf092f6b59b180516e4cc5a3375ad61ad9201f43f9f92272d@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dgfk2os48vuul"
 
 module.exports.hashSecret = "hashSecretText";
 
